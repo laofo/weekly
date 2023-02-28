7ea26c1b1442d4592cb5a1906a5549d2a6defec9
laofo <laofo521@gmail.com>
2023-03-01 00:25:20 +0800

    change theme

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  title: '安防科技',
+  author: '安防科技',
+  description: '安防科技产品专业厂家',
+  keywords: '安防，安防科技，防火涂料，军辉安防',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
-  repo: 'tw93/weekly'
+  homePage: 'https://anfangkeji.com',
+  blogPage: 'https://anfangkeji.com/',
+  twitterId: '',
+  githubId: '',
+  repo: '/'
 };
 
